a838a6ba10e0c988da1d070606d38c437a1c2ce2
GreatDeveloper66 <ashaffe@gmail.com>
2026-03-07 15:16:41 -0600

    updated error response handling in grocery store and supermarkets function

--- api/find-grocery-markets-within-radius.ts
+++ api/find-grocery-markets-within-radius.ts
@@ -3,21 +3,24 @@ import { findAllGroceryStoresAndSupermarketsWithinRadius } from "../services/con
 
 
 export default async function handler(
-  req: VercelRequest,
-  res: VercelResponse
+    req: VercelRequest,
+    res: VercelResponse
 ) {
     const { latitude, longitude, radius } = req.query;
     // Validate query parameters
     if (!latitude || !longitude || !radius) {
         return res.status(400).json({ error: "Missing required query parameters: latitude, longitude, radius" });
     }
-    if(parseInt(radius as string) > 50000) {
+    if (parseInt(radius as string) > 50000) {
         return res.status(400).json({ error: "Radius must be less than or equal to 50,000 meters" });
     }
     try {
         const groceryStoresAndSupermarkets = await findAllGroceryStoresAndSupermarketsWithinRadius(parseInt(radius as string), { latitude: parseFloat(latitude as string), longitude: parseFloat(longitude as string) });
         res.status(200).json({ status: "ok", places: groceryStoresAndSupermarkets });
     } catch (error) {
-        res.status(500).json({ error: "Failed to find grocery stores and supermarkets within radius", details: error instanceof Error ? error.message : "Unknown error" });
+        const responseStatus = res.statusCode;
+        const errorMessage = error instanceof Error ? error.message : "Unknown error";
+        console.error("Error in handler:", { status: responseStatus, error: errorMessage });
+        res.status(responseStatus).json({ error: "Failed to find grocery stores and supermarkets within radius", statusCode: responseStatus, details: errorMessage });
     }
 }
